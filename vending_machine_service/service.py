3e3abc34c8e252e6054bae4802efe4b8bec492b7
Tanadoon Lapanapornwong <tanadoon.lap@dome.tu.ac.th>
2021-12-22 21:18:11 +0700

    feat: refactor function calculate change coin function
    refactor from using dictionary to array to be more performance.

--- vending_machine_service/service.py
+++ vending_machine_service/service.py
@@ -22,16 +22,19 @@ class VendingMachineService:
     def _init_product_list(self):
         return [
             {
+                'barcode_id': '001',
                 'name': 'Pepsa non soda but za',
                 'name_th': 'เป๊ปซ่า น้ำไม่อัดลมแต่เสือกซ่า',
                 'price': 15,
                 'stock': 200
             }, {
+                'barcode_id': '002',
                 'name': 'Cake Not Coke but Cake',
                 'name_th': 'น้ำซ่าตราเค้ก',
                 'price': 20,
                 'stock': 2
             }, {
+                'barcode_id': '003',
                 'name': 'Peach Soda',
                 'name_th': 'น้ำซ่า 0 แคล รสพีช',
                 'price': 10,
@@ -57,23 +60,47 @@ class VendingMachineService:
 
         return [product for product in self.product_list if product.get('stock') > 0 and total_money > product.get('price')]
 
-    def change_coins(self, amount_change: int):
-        def _calculte_minimum_coin_to_change(amount_change: int = amount_change, coin_count: int = 0, coin_list: list = []):
+    def _create_change_coins_pool(self) -> list:
+        return [[coin_value, self.reserved_change_coins.get(f'{coin_value}', 0) + self.input_coins.get(f'{coin_value}', 0)] for coin_value in self.accept_coin[::-1]]
+
+
+    def calculate_change_coins(self, amount_change: int) -> dict:
+        self.min_coin = float("inf")
+        self.change_list = []
+        self._do_calculate_minimum_coin_to_change(amount_change=amount_change, change_coins_pool=self._create_change_coins_pool())
+        return {str(coin_value): self.change_list.count(coin_value) for coin_value in self.change_list}
+    
+    def _do_calculate_minimum_coin_to_change(self, amount_change: int, coin_count: int = 0, start: int = 0, coin_list: list = [], change_coins_pool: list = [[]]):
             if amount_change == 0 and self.min_coin > coin_count:
                 self.change_list = coin_list
                 self.min_coin = coin_count
             elif amount_change > 0:
-                for coin_value in self.accept_coin[::-1]:
-                    if self.reserved_change_coins.get(f'{coin_value}') > 0:
-                        self.reserved_change_coins.update({
-                            f'{coin_value}': self.reserved_change_coins.get(f'{coin_value}') - 1
-                        })
-                        _calculte_minimum_coin_to_change(amount_change-coin_value, coin_count+1, coin_list+[coin_value])
-                        self.reserved_change_coins.update({
-                            f'{coin_value}': self.reserved_change_coins.get(f'{coin_value}') + 1
-                        })
-
-        self.min_coin = float("inf")
-        self.change_list = []
-        _calculte_minimum_coin_to_change()
-        return {str(coin_value): self.change_list.count(coin_value) for coin_value in self.change_list}
+                for i in range(start, len(change_coins_pool)):
+                    if change_coins_pool[i][1] > 0:
+                        change_coins_pool[i][1] -= 1
+                        self._do_calculate_minimum_coin_to_change(
+                            amount_change=amount_change - change_coins_pool[i][0], 
+                            coin_count=coin_count + 1, 
+                            start=i,
+                            coin_list=coin_list + [change_coins_pool[i][0]],
+                            change_coins_pool=change_coins_pool,
+                        )
+                        change_coins_pool[i][1] += 1
+    
+    def reset_input_coin(self) -> None:
+        self.input_coins = self._init_input_coins()
+    
+    def update_reserved_change_coins(self, change_coins: dict, reset_input_coin_flag: bool = False) -> None:
+        for coin_value in self.accept_coin:
+            self.reserved_change_coins.update({
+                f'{coin_value}': (self.reserved_change_coins.get(f'{coin_value}', 0) + self.input_coins.get(f'{coin_value}', 0)) - change_coins.get(f'{coin_value}', 0)
+            })
+        if reset_input_coin_flag:
+            self.reset_input_coin()
+    
+    def update_product_stock_with_barcode_id(self, barcode_id: str, amount_change: int = -1) -> None:
+        for product in self.product_list:
+            if product.get('barcode_id') == barcode_id:
+                product.update({
+                    'stock': product.get('stock') + amount_change
+                })
